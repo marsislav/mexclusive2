9d47c4a78b61730afa84534470d017fe7c1532d4
marsislav <mkostadinov87@gmail.com>
2024-03-22 18:43:20 +0200

    Fixed bug

--- functions.php
+++ functions.php
@@ -5,25 +5,25 @@
  * @package mexclusive2
  */
 
-require_once( 'functions/dynamic-meta.php' );
+require_once( 'functions/block-patterns.php' );
 require_once( 'functions/class-wp-bootstrap-navwalker.php' );
+require_once( 'functions/config-theme-support.php' );
+require_once( 'functions/custom-background.php' );
 require_once( 'functions/customizer.php' );
-require_once( 'functions/sidebars.php' );
-require_once( 'functions/reading-time.php' );
-require_once( 'functions/posted-on.php' );
-require_once( 'functions/post-views-counter.php' );
+require_once( 'functions/editor-styles.php' );
+require_once( 'functions/enqueue.php' );
 require_once( 'functions/get-author.php' );
 require_once( 'functions/last-ten-posts-in-category.php' );
-require_once( 'functions/custom-background.php' );
-require_once( 'functions/random-order.php' );
-require_once( 'functions/woocommerce-support.php' );
-require_once( 'functions/editor-styles.php' );
 require_once( 'functions/popular-tags.php' );
-require_once( 'functions/block-patterns.php' );
-require_once( 'functions/enqueue.php' );
-require_once( 'functions/config-theme-support.php' );
+require_once( 'functions/post-views-counter.php' );
+require_once( 'functions/posted-on.php' );
+require_once( 'functions/random-order.php' );
+require_once( 'functions/reading-time.php' );
 require_once( 'functions/reply-comment.php' );
+require_once( 'functions/sidebars.php' );
 require_once( 'functions/woocommerce-cart.php' );
+require_once( 'functions/woocommerce-support.php' );
+require_once( 'functions/dynamic-meta.php' );
 
 
 
