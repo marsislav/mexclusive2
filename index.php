bc4f7736882e5ce30a7b2dc224ff09744d03649f
marsislav <mkostadinov87@gmail.com>
2024-03-27 20:35:55 +0200

    Fixed homepage ...

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
  * This is the most generic template file in a WordPress theme
  * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no template-home.php file exists.
+ * E.g., it puts together the home page when no home.php file exists.
  *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package mExlusive2
  */
@@ -14,131 +15,181 @@
 get_header(); ?>
     <div class="content-area">
         <main>
-            <div class="container" id="primary">
-                <div class="row">
-                    <div class="col">
-                        <ol class="breadcrumb justify-content-start mb-4">
-                            <li class="breadcrumb-item"><a
-                                        href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php _e( 'Home', 'mexclusive2' ); ?></a>
-                            </li>
-							<?php
-							$categories = get_the_category();
-							if ( ! empty( $categories ) ) {
-								$first_category = $categories[0]; // Get the first category
-								?>
-                                <li class="breadcrumb-item"><a
-                                            href="<?php echo esc_url( get_category_link( $first_category ) ); ?>"><?php echo esc_html( $first_category->name ); ?></a>
-                                </li>
-							<?php } ?>
-                            <li class="breadcrumb-item active text-dark"><?php the_title(); ?></li>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-            <section id="post-<?php the_ID(); ?>" <?php post_class('blog'); ?>>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-8">
-							<?php if ( have_posts() ):
-								while ( have_posts() ):the_post(); ?>
-                                    <article>
-                                        <h1><?php the_title(); ?></h1>
-                                        <div class="position-relative mb-2">
-											<?php if (has_post_thumbnail()) {
-												the_post_thumbnail( 'full', array( 'class' => 'img-fluid rounded' ) );
-											}
-											?>
-											<?php if (has_category()) {?>
-                                                <div class="position-absolute px-4 py-2 bg-primary rounded" style="top: 20px; right: 20px;">
-                                                    <a href="<?php echo esc_url(get_category_link($first_category)); ?>"><?php echo esc_html($first_category->name); ?></a>
-                                                </div>
-											<?php } ?>
-                                        </div>
-                                        <div class="d-flex justify-content-between post-info mb-2">
-                                            <i class="fa fa-clock"></i> <?php display_reading_time();?>
-											<?php mexclusive2_postedOn(); ?>
-                                            <i class="fa fa-eye"></i>
-											<?php
-											$views = mexclusive2_display_post_views();
-											printf(
-												_n('%d View', '%d Views', $views, 'mexclusive2'),
-												$views );
-											?>
-                                            <a href="#comments" class="text-dark link-hover me-3"><i class="fa fa-comment-dots"></i>
-												<?php
-												printf(
-													_n(
-														'%s Comment',
-														'%s Comments',
-														get_comments_number(),
-														'mexclusive2'
-													),
-													number_format_i18n(get_comments_number())
-												);
-												?>
-                                            </a>
-											<?php mexclusive2_authorInfo();?>
-                                        </div>
-                                        <div>
-											<?php the_content(); ?>
-                                        </div>
-                                        <div class="pagination-container">
-											<?php wp_link_pages( array(
-												'before'      => '<div class="pagination"><span class="page-links-title">' . __( 'Pages:', 'mexclusive2' ) . '</span>',
-												'after'       => '</div>',
-												'link_before' => '<span class="page-number">',
-												'link_after'  => '</span>',
-												'next_or_number' => 'number',
-												'nextpagelink' => __( 'Next page', 'mexclusive2' ),
-												'previouspagelink' => __( 'Previous page', 'mexclusive2' ),
-											) );?>
-                                        </div>
-                                        <hr>
-										<?php if (has_tag()) {?>
-                                            <div class="tab-class ">
-                                                <div class="d-flex justify-content-between border-bottom mb-4">
-                                                    <ul class="nav-pills d-inline-flex text-center">
-                                                        <li class="mb-3 ">
-                                                            <h5 class="mt-2 me-3 mb-0"><?php _e('Tags', 'mexclusive2') ?>:</h5>
-                                                        </li>
-														<?php
-														// Get tags for current post
-														$tags = get_the_tags();
-														if ($tags) {
-															// Loop through each tag
-															foreach ($tags as $tag) {?>
-                                                                <li class="nav-item mb-3">
-                                                                    <a class="d-flex py-2 bg-light rounded-pill me-2" href="<?php echo esc_url(get_tag_link($tag->term_id)); ?>">
-                                                        <span class="text-dark" style="width: 100px;">
-                                                            <?php echo $tag->name; ?>
-                                                        </span>
-                                                                    </a>
-                                                                </li>
-															<?php }
-														} ?>
-                                                    </ul>
-                                                </div>
-                                            </div>
-										<?php } ?>
+            <section class="slider">
 
-										<?php get_template_part('/template-parts/single/about-the-author'); ?>
-										<?php get_template_part('/template-parts/single/navigation'); ?>
-										<?php if (comments_open()) {comments_template();}?>
+                <div id="mainSlider" class="carousel slide mb-5" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+						<?php
+						// Loop through each slider page
+						for ( $i = 1; $i <= 3; $i ++ ) {
+							// Get slider page settings
+							$page_id     = get_theme_mod( 'set_slider_page' . $i );
+							$button_text = get_theme_mod( 'set_slider_button_text' . $i );
+							$button_url  = get_theme_mod( 'set_slider_url' . $i );
 
-                                    </article>
-								<?php
-								endwhile;
-							else :?>
-                                <p><?php _e('Nothing found', 'mexclusive2');?></p>
-							<?php endif;
+							// Output carousel item
+							$active_class = $i === 1 ? 'active' : ''; // Add 'active' class to the first item
 							?>
-                        </div>
-                        <div class="col-4">
-							<?php get_sidebar(); ?>
-                        </div>
+                            <div class="carousel-item <?php echo $active_class; ?>">
+								<?php if ( $page_id ) : ?>
+
+									<?php echo get_the_post_thumbnail( $page_id, 'large', array(
+										'class' => 'd-block w-100',
+										'alt'   => get_the_title( $page_id )
+									) ); ?>
+
+									<?php if ( $button_text && $button_url ) : ?>
+                                        <div class="carousel-caption">
+                                            <h5><?php echo esc_html( get_the_title( $page_id ) ); ?></h5>
+                                            <p><?php echo esc_html( get_the_excerpt( $page_id ) ); ?></p>
+                                            <a href="<?php echo esc_url( $button_url ); ?>"
+                                               class="btn btn-primary mt-5"><?php echo esc_html( $button_text ); ?></a>
+                                        </div>
+									<?php endif; ?>
+								<?php endif; ?>
+                            </div>
+						<?php } ?>
                     </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#mainSlider"
+                            data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden"><?php _e( 'Previous', 'mexclusive2' ); ?></span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#mainSlider"
+                            data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden"><?php _e( 'Next', 'mexclusive2' ); ?></span>
+                    </button>
                 </div>
             </section>
+			<?php if ( class_exists( 'WooCommerce' ) ): ?>
+				<?php
+				$showdeal = get_theme_mod( 'set_deal_show', 0 );
+				$deal     = get_theme_mod( 'set_deal' );
+				$currency = get_woocommerce_currency_symbol();
+				$regular  = get_post_meta( $deal, '_regular_price', 'true' );
+				$sale     = get_post_meta( $deal, '_sale_price', 'true' );
+
+				if ( $showdeal == 1 && ( ! empty( $deal ) ) ) :
+					$discount_percentage = absint( 100 - ( $sale / $regular ) * 100 );
+					?>
+                    <section class="deal-of-the-week mb-5" id="primary">
+                        <div class="container">
+
+                            <div class="row d-flex align-items-center">
+
+                                <div class="deal-img col-md-6 col-12 ms-auto text-center">
+									<?php
+									echo get_the_post_thumbnail( $deal, 'large', array( 'class' => 'img-fluid rounded' ) );
+									?>
+                                </div>
+                                <div class="deal-desc col-md-4 col-12 me-auto text-center">
+                                    <h2><?php _e( 'Deal of the week', 'mexclusive2' ); ?></h2>
+									<?php if ( ! empty( $sale ) ): ?>
+                                        <span class="discount">
+
+                                <?php echo '- ' . $discount_percentage . '%'; ?>
+                            </span>
+									<?php endif; ?>
+                                    <h3>
+                                        <a href="<?php echo get_permalink( 'deal' ); ?>"><?php echo get_the_title( 'deal' ); ?></a>
+                                    </h3>
+                                    <p>
+										<?php echo get_the_excerpt( $deal ); ?>
+                                    </p>
+                                    <div class="prices">
+                                <span class="regular">
+                                </span>
+										<?php if ( ! empty( $sale ) ): ?>
+                                            <span class="sale">
+                                        <?php echo $sale . ' ' . $currency; ?>
+                                    </span>
+										<?php endif; ?>
+                                    </div>
+                                    <a href="<?php echo esc_url( '?add-to-cart=' . $deal ); ?>"
+                                       class="btn btn-primary"><?php _e( 'Add to cart', 'mexclusive2' ); ?></a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </section>
+				<?php endif; ?>
+
+
+                <section class="popular-products">
+                    <div class="container">
+						<?php
+						$popular_limit  = get_theme_mod( 'set_popular_max_num', 4 );
+						$popular_col    = get_theme_mod( 'set_popular_max_col', 4 );
+						$arrivals_limit = get_theme_mod( 'set_new_arrivals_max_num', 4 );
+						$arrivals_col   = get_theme_mod( 'set_new_arrivals_max_col', 4 );
+
+						?>
+
+                        <h2><?php _e('Popular Products', 'mexclusive2');?></h2>
+						<?php echo do_shortcode( '[products limit=" ' . $popular_limit . ' " columns="' . $popular_col . '" orderby="popularity"]' ); ?>
+                    </div>
+                </section>
+                <section class="new-arrivals">
+                    <div class="container">
+                        <h2><?php _e('New Arrivals', 'mexclusive2');?></h2>
+						<?php echo do_shortcode( '[products limit="' . $arrivals_limit . '" columns="' . $arrivals_col . '" orderby="date"]' ); ?>
+                    </div>
+                </section>
+			<?php endif; ?>
+            <section class="blog">
+                <div class="container">
+                    <div class="row">
+						<?php if ( have_posts() ):
+							while ( have_posts() ):the_post(); ?>
+                                <article class="col-12 col-md-6 home-img position-relative mb-30">
+									<?php
+									$categories = get_the_category();
+									if ( ! empty( $categories ) ) {
+										$first_category = $categories[0]; // Get the first category
+									}
+									?>
+									<?php if ( has_category() ) { ?>
+                                        <div class="position-absolute px-4 py-2 bg-primary rounded"
+                                             style="top: 20px; right: 20px;">
+                                            <a href="<?php echo esc_url( get_category_link( $first_category ) ); ?>"><?php echo esc_html( $first_category->name ); ?></a>
+                                        </div>
+									<?php } ?>
+
+
+                                    <a href="<?php the_permalink(); ?>">
+										<?php if ( has_post_thumbnail() ) {
+											the_post_thumbnail( 'full', array( 'class' => 'img-fluid rounded' ) );
+										}
+										?>
+                                    </a>
+                                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                                    <div class="excerpt"><?php the_excerpt(); ?></div>
+                                </article>
+
+							<?php
+							endwhile;
+						else :?>
+                            <p><?php _e( 'Nothing found', 'mexclusive2' ); ?></p>
+						<?php endif;
+						?>
+                    </div>
+                </div>
+                <!-- Add pagination here -->
+
+                <div class="pagination mb-5">
+					<?php the_posts_pagination( array(
+						'prev_text' =>  __( '&laquo;', 'mexclusive2' ),
+						'next_text' => __( '&raquo;', 'mexclusive2' ),
+					) ); ?>
+                </div>
+
+            </section>
+
+
+
+
+
         </main>
     </div>
 <?php get_footer(); ?>
